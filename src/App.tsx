65e6188176fa6ab4bf8054713e1d1151c863a55e
Ippei1111 <ippei523@i.softbank.jp>
2021-07-01 11:26:57 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  { useState }  from "react";
+import "./App.css"
 
-function App() {
+type Props = {
+  title?: string;
+  initValue: number;
+}
+
+const App: React.FC<Props> = ({title, initValue}: Props) => {
+  const [count, setCount] = useState(initValue || 3)
+
+  const onAddEvent = () => {
+    setCount(count + 1)
+  }
+
+  const onSubEvent = () => {
+    setCount(count - 1)
+  }
+
+  const onMulEvent = () => {
+    setCount(count * 2)
+  }
+
+  const onDivideEvent = () => {
+    setCount(count / 2)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <h1>{title || "Counter App"}</h1>
+      <div className="button_wrap">
+        <button onClick={onMulEvent} className="button_child">×2</button>
+        <button onClick={onAddEvent} className="button_child">+</button>
+        <p className="counter">{count}</p>
+        <button onClick={onSubEvent} className="button_child">-</button>
+        <button onClick={onDivideEvent} className="button_child">÷2</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
